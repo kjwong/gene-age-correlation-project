3f0ca95fcf0f230653ee85389d24f14cd3f2ef83
kwong95 <kenny22591707@gmail.com>
2016-04-21 04:05:41 -0400

    Delete makedefault.R

--- makedefault.R
+++
@@ -1,6 +0,0 @@
-# creation of default
-sample_f<-read.table("sample_f.csv", header=T, sep = ",", row.names=1)
-GSE58137.samples<-read.table("GSE58137.samples.txt", header=T, sep = "\t", row.names=1)
-GSE58137.entrez <- data.frame(fread("GSE58137.entrez.pcl", header = T,sep="\t"), row.names = 1)
-blood.f <- data.frame(fread("blood.f.pcl", header = T,sep="\t"), row.names = 1)
-save.image("default.RData")
